ce4ef03c79361eaa524c8411d9c5ec0112450d95
sinethrenuja1 <sinethrenuja312@gmail.com>
2024-03-20 09:16:44 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const db= require('./models');
+
+// //Routers
+const postRouter = require('./routes/posts');
+app.use('/posts', postRouter);
+
+db.sequelize.sync().then(() => {;
+  app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+  });
+});
